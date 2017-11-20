b97ef4b4e1c31f2e6c515babeda199ca67a7b99c
inyoungW <wang9403@naver.com>
2017-11-20 12:45:33 +0900

    Test

--- app/src/main/java/com/famous/restaurant/TestActivity.java
+++ app/src/main/java/com/famous/restaurant/TestActivity.java
@@ -3,14 +3,11 @@ package com.famous.restaurant;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("hello");
+        setContentView(R.layout.activity_test);
     }
 }
