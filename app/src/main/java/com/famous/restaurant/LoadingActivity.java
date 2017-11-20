6051fe15a9dac5afe1ce4b68611e9869a4aac845
BuildTheTruth <33617083+BuildTheTruth@users.noreply.github.com>
2017-11-20 12:46:42 +0900

    loadingActivity

---
+++ app/src/main/java/com/famous/restaurant/LoadingActivity.java
@@ -0,0 +1,46 @@
+package com.famous.restaurant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class LoadingActivity extends AppCompatActivity {
+    ImageView iv_appName;
+    ImageView iv_loadingLogo;
+    Animation loadingAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        iv_appName=(ImageView)findViewById(R.id.iv_appName);
+        iv_loadingLogo=(ImageView)findViewById(R.id.iv_loadingLogo);
+
+        loadingAnim= AnimationUtils.loadAnimation(this, R.anim.loading);
+        loadingAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+
+        iv_loadingLogo.startAnimation(loadingAnim);
+        iv_appName.startAnimation(loadingAnim);
+
+    }
+}
