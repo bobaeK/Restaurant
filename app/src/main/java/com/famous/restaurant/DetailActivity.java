a3f40fcdab27db99f255876b798699cfa2510320
BOBAE <USER@DESKTOP-N04F945>
2017-12-10 04:03:08 +0900

    매니페스트 gps권한 추가 상세보기 화면 이미지 ListView 관련 파일 추가 및 액티비티 수정

--- app/src/main/java/com/famous/restaurant/DetailActivity.java
+++ app/src/main/java/com/famous/restaurant/DetailActivity.java
@@ -1,10 +1,18 @@
 package com.famous.restaurant;
 
-import android.app.FragmentTransaction;
-import android.support.v4.app.FragmentManager;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -12,6 +20,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
 
         //이 조건문 왜 쓰는지 모름,,
         if (savedInstanceState == null) {
@@ -21,9 +30,75 @@ public class DetailActivity extends AppCompatActivity {
                     .replace(R.id.map_fragment, detailMapFragment, "detail_map")
                     .commit();
         }
+        //이미지 ListView
+
 
     }
-    public void onTotalReviewClicked(View view){
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    //지도확대
+    public void onMapClicked(){
+
+    }
+    //전화걸기
+    public void onPhoneCallClicked(View view){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(myIntent);
+    }
+    //사진확대
+    public void onImageClicked(View view){
+
+    }
+    //전체후기보기
+    public void onTotalReviewClicked(View view){
+        Intent intent = new Intent(this, TotalReviewActivity.class);
+        startActivity(intent);
+    }
+    //인증하기
+    public void onAddPlaceClicked(View view){
+
+    }
+    //후기등록
+    public void onAddReviewClicked(View view){
+
+    }
+    //뒤로가기
+    public void onBackButtonClicked(View view){
+        finish();
+    }
+    //상세보기화면 이미지 리스트뷰 어답터
+    private class DetailImagedapter extends BaseAdapter {
+        private ArrayList<DetailImageItem> items = new ArrayList<DetailImageItem>();
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            DetailPictureItemView view = (DetailPictureItemView) convertView;
+
+            if(convertView == null)
+                view = new DetailPictureItemView(getApplicationContext());
+            DetailImageItem item = items.get(position);
+            view.setImage(item.getImage());
+            return view;
+        }
+        void addItem(DetailImageItem item){ items.add(item); }
     }
 }
---
+++ app/src/main/java/com/famous/restaurant/DetailImageItem.java
@@ -0,0 +1,20 @@
+package com.famous.restaurant;
+
+/**
+ * Created by BOBAE on 2017-12-09.
+ */
+
+public class DetailImageItem {
+    int image;
+
+    public DetailImageItem(int image) {
+        this.image = image;
+    }
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
